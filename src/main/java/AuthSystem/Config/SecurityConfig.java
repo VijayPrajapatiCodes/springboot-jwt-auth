5988caae5918ab831fc279da981646e649184830
Vijay Prajapati <vijayprajapati24645@gmail.com>
2026-03-01 19:16:13 +0530

    Fix auth path matcher

--- src/main/java/AuthSystem/Config/SecurityConfig.java
+++ src/main/java/AuthSystem/Config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
             .cors(cors -> {})   // Enable CORS
 
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("api/auth/**").permitAll()
+                    .requestMatchers("/api/auth/**").permitAll()
                     .anyRequest().authenticated()
             )
 
